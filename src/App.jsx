3461f716df304387776b87fc9f87c45ff5aed957
tocteman <juanfrancisco.vinueza@gmail.com>
2022-08-29 13:33:56 -0500

    remove scroll observer from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import { useState, useEffect } from 'react'
 import './App.css'
 import Menu from './components/Menu.jsx'
 import Welcome from './components/Welcome.jsx'
 import Features from './components/Features.jsx'
 
 function App() {
-
-  const handleScroll = e => {
-    console.log("algo")
-    console.log(e.currentTarget.scrollTop)
-    console.log(e.currentTarget.offsetHeight)
-  }
-
 
   return (
     <div className="page">
